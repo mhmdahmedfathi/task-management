abba997427846da02fd7901e2bd46afa3b65f6a3
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2025-02-08 03:06:17 +0200

    --feat ( replace static data with data from firebase and make it subscribed to change by changing the data )

--- src/components/Pages/task-managment.tsx
+++ src/components/Pages/task-managment.tsx
@@ -1,44 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useTableColumns from '../ui/table/columns';
 import TasksTable from '../ui/table/table';
 import { AccessorKeyColumnDef } from '@tanstack/react-table';
 import Header from '../ui/Header';
+import taskService from '../../services/taskService';
 
 
 const TaskManager: React.FC = () => {
 
-  const [tasks, setTasks] = useState<TTasks[]>([
-    {
-      id: '1',
-      title: 'Task 1',
-      description: '14 This is a description for Task 1',
-      dueDate: '2025-12-31',
-      status: 'Pending',
-    },
-    {
-      id: '2',
-      title: 'Task 2',
-      description: '3 This is a description for Task 2',
-      dueDate: '2022-12-31',
-      status: 'Pending',
-    },
-    {
-      id: '3',
-      title: 'Task 3',
-      description: ' 4 This is a description for Task 3',
-      dueDate: '2023-12-31',
-      status: 'Pending',
-    },
-  ]);
+  const [tasks, setTasks] = useState<TTasks[]>([]);
 
-  const columns: AccessorKeyColumnDef<TTasks, string>[] = useTableColumns()
+  useEffect(() => {
+    taskService.onTaskUpdate((tasks) => {
+      setTasks(tasks);
+    });
+  }, []);
+
+  const columns = useTableColumns() as AccessorKeyColumnDef<TTasks, "Pending" | "Completed" | "Overdue">[]
 
   return (
     <div className="min-h-screen w-screen bg-gray-100">
       <Header />
       <div className="p-4">
-            <TasksTable columns={columns} data={tasks} />
+          <TasksTable columns={columns} data={tasks} />
       </div>
     </div>
   );
